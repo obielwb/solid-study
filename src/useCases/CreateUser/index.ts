bd24ddcc2a02c7a626e9780b652cad70ae20624e
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2022-01-07 13:35:12 -0300

    feat(api): add user creation functionality

---
+++ src/useCases/CreateUser/index.ts
@@ -0,0 +1,18 @@
+import { MailtrapMailProvider } from "../../providers/implementations/MailtrapMailProvider";
+import { PostgresUserRepository } from "../../repositories/implementations/PostgresUsersRepository";
+import { CreateUserController } from "./CreateUserController";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+const mailtrapMailProvider = new MailtrapMailProvider();
+const postgresUserRepository = new PostgresUserRepository();
+
+const createUserUseCase = new CreateUserUseCase(
+  postgresUserRepository, 
+  mailtrapMailProvider
+)
+
+const createUserController = new CreateUserController(
+  createUserUseCase
+)
+
+export { createUserUseCase, createUserController }
